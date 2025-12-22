52a4eeba4ca30c0c312d4d7d1da58f61a480fc43
M001woo <micketx@gmail.com>
2025-12-22 14:21:32 -0800

    image

--- script.js
+++ script.js
@@ -165,7 +165,7 @@ async function loadSectionBackgrounds() {
                             url('${imagePath}')
                         `;
                         heroSection.style.backgroundSize = 'cover';
-                        heroSection.style.backgroundPosition = '50% 45%';
+                        heroSection.style.backgroundPosition = '50% 25%';
                         heroSection.style.backgroundAttachment = 'fixed';
                         console.log(`Applied background to hero: ${imagePath}`);
                     }
